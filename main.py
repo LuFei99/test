02e10e124d593fb0430c421bd1b487d897d67cde
LuFei99 <43195193+LuFei99@users.noreply.github.com>
2022-10-14 15:08:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 import random
 
 today = datetime.now()
-start_date ='2022-10-10' #os.environ['START_DATE']
-city ='抚州' #os.environ['CITY']
+start_date ='2021-12-17' #os.environ['START_DATE']
+city =os.environ['CITY']
 birthday ='12-23' #os.environ['BIRTHDAY']
 
 app_id = os.environ["APP_ID"]
